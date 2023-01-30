ba87ae2e0181789db0f1fb4e7f85d25039f9cfba
HobumPark <gloryofalive@naver.com>
2023-01-30 16:29:19 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import axios from 'axios';
+
+function App() {
+
+  const getData=async()=>{
+    //axios.defaults.baseURL = 'http://mytest.cafe24app.com';
+    axios.defaults.headers.get['Content-Type'] ='application/json;charset=utf-8';
+    axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+    axios.defaults.headers.get['widthCredentials'] = true
+    const result=await axios.get('/board')
+    console.log(result)
+  }
+
+  const getData2=async()=>{
+    const result=await axios.get('/board/2')
+    console.log(result)
+  }
+
+  return (
+    <div className="App">
+        <button onClick={getData}>요청</button>
+        <button onClick={getData2}>요청</button>
+    </div>
+  );
+}
+
+export default App;
